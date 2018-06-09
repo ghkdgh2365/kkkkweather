b0dee8a7303986e439b17af3583e1efc679f01a8
ghkdgh2365 <ghkdgh2365@gmail.com>
2018-06-09 23:47:58 +0000

    자동응답

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'home/searchStationId'
   get 'home/weather'
   get 'home/arriveData'
+  get '/keyboard' => 'basic#keyboard_init'
+  post '/message' => 'basic#chat_control'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
