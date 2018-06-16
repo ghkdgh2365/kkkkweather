d137e775b42538c7b5a2597a33ba1fd1f065647f
ghkdgh2365 <ghkdgh2365@gmail.com>
2018-06-16 07:51:08 +0000

    test message

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'home/arriveData'
   get '/keyboard' => 'basic#keyboard_init'
   post '/message' => 'basic#chat_control'
+  post '/message' => 'basic#chat_control2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
