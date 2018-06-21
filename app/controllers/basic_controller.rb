9d2affd2f65d6e8fffcb994949cea8ec871c5994
ghkdgh2365 <ghkdgh2365@gmail.com>
2018-06-21 15:33:12 +0000

    button region

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -84,10 +84,10 @@ class BasicController < ApplicationController
               },
               keyboard: {
                 type: "buttons",
-                buttons: ["서울", "수원", "파주", "인천", "강릉", "원주", "춘천"]
+                buttons: ["서울", "수원", "파주", "인천", "강릉", "원주", "춘천", "대전", "홍성", "세종", "청주", "군산", "광주", "목포", "여수", "전주", "대구", "부산", "안동", "울산", "창원", "포항"]
               }
             }
-            # , "대전", "홍성", "세종", "청주", "군산", "광주", "목포", "여수", "전주", "대구", "부산", "안동", "울산", "창원", "포항", "서귀포", "제주"
+            # , "서귀포", "제주"
             render json: @msg, status: :ok
         end
         if @response == "서울" or @response == "수원" or @response == "파주" or @response == "인천" or @response == "강릉" or @response == "원주" or @response == "춘천" or @response == "대전" or @response == "홍성" or @response == "세종" or @response == "청주" or @response == "군산" or @response == "광주" or @response == "목포" or @response == "여수" or @response == "전주" or @response == "대구" or @response == "부산" or @response == "안동" or @response == "울산" or @response == "창원" or @response == "포항"
